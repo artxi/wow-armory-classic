048983340a4f86f3f61b0e748467fa12ccd096c5
artxi <aartxi@gmail.com>
2020-04-02 14:24:19 +0200

    remove socket stuff

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ try {
 
   const Express = require('express');
   const App = Express();
-  const Server = require('http').Server(App);
-  const Io = require('socket.io')(Server);
   const Path = require('path');
 
   const Settings = require('./config/settings');
@@ -17,31 +15,14 @@ try {
 
   //WarcraftLogs.requestCharacterGear('Basati', 'Mograine', 'eu');
 
-  Io.on('connection', socket => {
-    setSocketEvents(socket);
-  });
-
-  function setSocketEvents(socket) {
-    Logger.log('User connected to webview');
-    // socket
-    //   .on('getCurrentTargets', () => {
-    //     socket.emit('currentTargets', Twitter.getCurrentTargets());
-    //   })
-    //   .on('addNewTarget', data => {
-    //     Twitter.addUserData(data);
-    //   })
-    //   .on('deleteTarget', name => {
-    //     Twitter.deleteTarget(name);
-    //   });
-  }
-
   App.get('/', (req, res) => {
     res.sendFile(Path.resolve('public/views/index.html'));
+    Logger.log('User connected to webview');
   });
 
   App.use(Express.static('public'));
 
-  Server.listen(Settings.port || 3000);
+  App.listen(Settings.port || 3000);
 } catch (e) {
   Logger.error(e);
 }
