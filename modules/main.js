33c02f16de6a78dff283cfd3b503e5d5c1a9ccba
artxi <aartxi@gmail.com>
2020-05-10 16:43:21 +0200

    Change ZG enchants on csv

--- modules/main.js
+++ modules/main.js
@@ -99,17 +99,58 @@ module.exports = {
       const gearSet = character.gearSets.filter(s => s.bossId === 612).sort((a, b) => b.date - a.date)[0];
 
       for (const slot of slots) {
-        const item = gearSet.items.find(g => g.slot === slot);
+        const item = gearSet.items[slot];
         if (item) {
           gearLine[slot] = item.name;
           if (item.enchant) {
-            enchantsLine[slot] = item.enchant.name;
+            let name;
+
+            switch (item.enchant.id) {
+              case 2583:
+                name = '+10 Sta / +7 Def / +15 Block';
+                break;
+              case 2584:
+                name = '+10 Sta / +7 Def / +24 SP';
+                break;
+              case 2585:
+                name = '+28 AP / +1% Dodge';
+                break;
+              case 2586:
+                name = '+24 AP / +10 Sta / +1% Hit';
+                break;
+              case 2588:
+                name = '+18 SP / +1% Hit';
+                break;
+              case 2589:
+                name = '+18 SP / +10 Sta';
+                break;
+              case 2590:
+                name = '+10 Sta / +4MP5 / +24 SP';
+                break;
+              case 2591:
+                name = '+10 Sta / +10 Int / +24 SP';
+                break;
+              case 2604:
+                name = '+33 Healing Spells';
+                break;
+              case 2605:
+                name = '+18 Spell Power';
+                break;
+              case 2606:
+                name = '+30 AP';
+                break;
+              default:
+                name = item.enchant.name;
+                break;
+            }
+
+            enchantsLine[slot] = name;
           } else {
-            enchantsLine[slot] = '';
+            enchantsLine[slot] = ' ';
           }
         } else {
           gearLine[slot] = '';
-          enchantsLine[slot] = '';
+          enchantsLine[slot] = ' ';
         }
       }
 
